0de408d02414b147264e01212f367d0fb655c754
崔国程 <nanhaierbuku@qq.com>
2018-01-06 11:10:54 +0800

    测试注释:测试是否能上传到GitHub

--- src/main/java/com/lanou3g/login/controller/login.java
+++ src/main/java/com/lanou3g/login/controller/login.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class login {
 
+    //登录页面 测试注释
     @RequestMapping("/login")
     public String login(){
         return "login";
