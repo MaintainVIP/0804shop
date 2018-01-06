828497172d9561e296a2dc7fb679a32581a10331
liweiwei <zerofire.li@qq.com>
2018-01-06 11:53:46 +0800

    测试

--- src/main/java/com/lanou3g/login/controller/login.java
+++ src/main/java/com/lanou3g/login/controller/login.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class login {
-
+//我考的风格
     @RequestMapping("/login")
     public String login(){
         return "login";
